b80f4e426d1317422f928cc8031f3b0a70cb255f
wopoadi <kodjo@osekoo.com>
2025-01-17 15:41:53 +0100

    Add kafka image

--- kafka/3/start.sh
+++ kafka/3/start.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-# Start Kafka in KRaft mode
+# Start Zookeeper
+/opt/zookeeper/bin/zkServer.sh --config /opt/zookeeper/conf start-foreground &
+
+# Wait for Kafka to be ready
+echo "Waiting for Zookeeper to be ready..."
+while ! nc -z localhost 2181; do
+  echo "Zookeeper is not ready yet. Retrying..."
+  sleep 1
+done
+
+# Format Kafka storage
 /opt/kafka/bin/kafka-storage.sh format \
   --config /opt/kafka/config/server.properties \
-  --cluster-id $(/opt/kafka/bin/kafka-storage.sh random-uuid)
+  --cluster-id "$(/opt/kafka/bin/kafka-storage.sh random-uuid)"
 
+# Start Kafka in KRaft mode
 /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties &
 
-# Attendre que Kafka démarre
+# Wait for Kafka to be ready
 echo "Waiting for Kafka to be ready..."
 while ! nc -z localhost 9092; do
   echo "Kafka is not ready yet. Retrying..."
@@ -16,5 +27,5 @@ done
 
 echo "Kafka is ready. Starting Kafdrop."
 
-# Démarrer Kafdrop
+# Start Kafdrop in daemon mode to monitor Kafka
 java -jar /opt/kafdrop.jar --kafka.brokerConnect=localhost:9092 --server.port=9000
